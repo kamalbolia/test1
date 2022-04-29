da0d2c213d06a56b53205c6fca1a6925566d4909
kamalbolia <85377749+kamalbolia@users.noreply.github.com>
2022-04-28 23:48:58 -0500

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,10 +1,3 @@
-pipeline {
-    agent any 
-    stages {
-        stage('Stage 1') {
-            steps {
+
                 echo 'Hello world! pipeline' 
-            }
-        }
-    }
-}
+          
